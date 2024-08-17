b7a57e5b5b8eb9fe28a46ebf21c5a70992637de0
Maulin S <shahmaulin1@gmail.com>
2024-08-17 13:18:17 +0530

    Create hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,10 @@
+import datetime
+def main():
+  print("Hello World!")
+
+#print current datetime
+  current_time = datetime.datetime.now()
+  print("current_time:" ,current_time)
+
+if__name__== "__main__"
+  main()
